516da3d13e930fbbfb65e2791eddff6b28847714
aniruddha.humane <aniruddha.humane@outlook.com>
2023-05-07 02:25:08 +0900

    Add Netflix Skipper

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Netflix-Skipper
-Automatically skip Netflix intros and jump to the next episode
+Automatically skip Netflix intros and jump to the next episode.
+
+## Installation
+### Automatic
+1. Download from chrome web store!
+### Manual
+1. Download the repository as zip and unzip the zip file.
+2. Open Chrome and navigate to chrome://extensions/.
+3. Enable "Developer mode" by toggling the switch in the top right corner.
+4. Click "Load unpacked" and select the folder containing your extension files (netflix-skipper).
+5. Your extension should now appear in the list, and you should see the icon in your Chrome toolbar.
---
+++ popup.js
@@ -0,0 +1,41 @@
+const skipIntroToggle = document.getElementById("skipIntro");
+const nextEpisodeToggle = document.getElementById("nextEpisode");
+
+function updateSetting(key, value) {
+  chrome.storage.local.set({ [key]: value });
+}
+
+function sendMessage(action, value, retry = 3) {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    if (tabs[0].url.includes("netflix.com")) {
+      chrome.tabs.sendMessage(tabs[0].id, { action, value }, (response) => {
+        if (chrome.runtime.lastError) {
+          if (retry > 0) {
+            setTimeout(() => sendMessage(action, value, retry - 1), 500);
+          } else {
+            console.error(chrome.runtime.lastError.message);
+          }
+        }
+      });
+    }
+  });
+}
+
+skipIntroToggle.addEventListener("change", (event) => {
+  updateSetting("skipIntro", event.target.checked);
+  console.log("toggleSkipIntro", event.target.checked);
+  sendMessage("toggleSkipIntro", event.target.checked);
+});
+
+nextEpisodeToggle.addEventListener("change", (event) => {
+  updateSetting("nextEpisode", event.target.checked);
+  console.log("toggleNextEpisode", event.target.checked);
+  sendMessage("toggleNextEpisode", event.target.checked);
+});
+
+chrome.storage.local.get(["skipIntro", "nextEpisode"], (result) => {
+  skipIntroToggle.checked = result.skipIntro !== false;
+  nextEpisodeToggle.checked = result.nextEpisode !== false;
+  sendMessage("toggleSkipIntro", skipIntroToggle.checked);
+  sendMessage("toggleNextEpisode", nextEpisodeToggle.checked);
+});
